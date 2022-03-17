41f89f4bb002a97938ef9caa2fee27c5a036b7c7
robotTX1 <robottx3@gmail.com>
2022-03-17 15:33:11 +0100

    Refactoring to a common interface. Closes #10

--- pet-clinic-data/src/main/java/rtx/spring/rtxpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/rtx/spring/rtxpetclinic/services/OwnerService.java
@@ -2,14 +2,6 @@ package rtx.spring.rtxpetclinic.services;
 
 import rtx.spring.rtxpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/rtx/spring/rtxpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/rtx/spring/rtxpetclinic/services/PetService.java
@@ -2,12 +2,5 @@ package rtx.spring.rtxpetclinic.services;
 
 import rtx.spring.rtxpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
