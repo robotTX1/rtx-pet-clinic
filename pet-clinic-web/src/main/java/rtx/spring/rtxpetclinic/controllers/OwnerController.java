4442bec0720ce942ebe52005d9342868d7132197
robotTX1 <robottx3@gmail.com>
2022-03-18 22:44:53 +0100

    Added Owner list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/rtx/spring/rtxpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/rtx/spring/rtxpetclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package rtx.spring.rtxpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rtx.spring.rtxpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owner/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
