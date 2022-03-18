523ab0e9bea6f8f3d71f742b7dd11283d9294ad8
robotTX1 <robottx3@gmail.com>
2022-03-18 22:57:02 +0100

    Added Pet list and updated controller. Closes #14

--- pet-clinic-web/src/main/java/rtx/spring/rtxpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/rtx/spring/rtxpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package rtx.spring.rtxpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rtx.spring.rtxpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
