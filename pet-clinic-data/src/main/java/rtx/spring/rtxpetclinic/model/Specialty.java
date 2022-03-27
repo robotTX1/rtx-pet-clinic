de959cfffed3cdc8233ee2f4ca51fc12cb2ed01f
robotTX1 <robottx3@gmail.com>
2022-03-27 16:44:49 +0200

    Added Vet Specialty and added it to Vet. Closes #26

---
+++ pet-clinic-data/src/main/java/rtx/spring/rtxpetclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package rtx.spring.rtxpetclinic.model;
+
+public class Specialty extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
