82e86a948b1b042a4d462089fa4e22187caa58eb
robottx3 <robottx3@gmail.com>
2022-04-02 23:05:35 +0200

    Added Spring Data JPA for Owner. Closes #39

---
+++ pet-clinic-data/src/main/java/rtx/spring/rtxpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,53 @@
+package rtx.spring.rtxpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import rtx.spring.rtxpetclinic.model.Owner;
+import rtx.spring.rtxpetclinic.repositories.OwnerRepository;
+import rtx.spring.rtxpetclinic.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
